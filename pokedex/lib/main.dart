43ad10a8272180c64aa96dfcb19e6836c1d61a5d
Nikolas-Mendes <nikolaspoi055@gmail.com>
2021-08-11 16:44:45 -0300

    Terminado Projeto Pokedex

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pokedex/screens/home/home_page.dart';
 
 void main() {
+  // Main
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
